bbdded7d2196d9ae19ef3cea315bc6d6401d1db5
Johannes Schickling <schickling.j@gmail.com>
2016-05-12 14:30:55 +0200

    init

---
+++ graphqlclient/client.py
@@ -0,0 +1,4 @@
+
+
+def query():
+    return "test"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graphqlclient',
+      version='0.1.0',
+      description='Simple GraphQL client for Python 2.7+',
+      url='https://github.com/graphcool/python-graphql-client',
+      author='graph.cool',
+      author_email='hello@graph.cool',
+      license='MIT',
+      packages=['graphqlclient'],
+      zip_safe=False)
