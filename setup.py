79d2a6d360ad717a9f6241d98be7b99124a6217f
Johannes Schickling <schickling.j@gmail.com>
2017-09-21 12:20:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='graphqlclient',
-      version='0.1.2',
+      version='0.2.0',
       description='Simple GraphQL client for Python 2.7+',
       url='https://github.com/graphcool/python-graphql-client',
       author='graph.cool',
