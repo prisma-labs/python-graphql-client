bd34326379f701693a17ff79fe0605f992d15c08
Sunil Shah <ssshah86@users.noreply.github.com>
2018-06-30 17:25:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='graphqlclient',
-      version='0.2.1',
+      version='0.2.2',
       description='Simple GraphQL client for Python 2.7+',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      url='https://github.com/graphcool/python-graphql-client',
+      url='https://github.com/prismagraphql/python-graphql-client',
       author='prisma.io',
       author_email='hello@prisma.io',
       license='MIT',
