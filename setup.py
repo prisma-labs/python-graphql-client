7eecc878c4d10a6a52d9890794981979bf539662
ssshah86 <ssshah86@gmail.com>
2018-07-12 04:38:48 +0200

    increment setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='graphqlclient',
-      version='0.2.3',
+      version='0.2.4',
       description='Simple GraphQL client for Python 2.7+',
       long_description=long_description,
       long_description_content_type='text/markdown',
